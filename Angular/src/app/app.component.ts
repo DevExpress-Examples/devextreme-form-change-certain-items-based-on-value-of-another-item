0dc612a4ac1260ba32c989b7eb68bdd9755bfba4
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 13:21:31 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxFormModule, DxFormTypes } from 'devextreme-angular/ui/form';
 import { Employee, Service } from './app.service';
 
@@ -7,6 +7,7 @@ import { Employee, Service } from './app.service';
     imports: [DxFormModule],
     providers: [Service],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
